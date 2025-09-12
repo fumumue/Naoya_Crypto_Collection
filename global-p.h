152549878ad30bb0bdb0b68d5eb8bc013a65fdda
anang0g0 <test@debian>
2025-09-13 03:17:06 +0900

    1129-64

--- global-p.h
+++ global-p.h
@@ -5,10 +5,10 @@
 //符号のパラーメータの指定。通常[N,K,T]として、
 //Nは符号の長さ、Kが符号の次元、Tは訂正エラー数
 //を表す。ここではDは符号長にしている。
-#define N 17 // set small prime ex. p=2053
+#define N 1129 // set small prime ex. p=2053
 #define M N // puncture code
-#define K (8) // degree of polynomial
-#define E (5)   // bit size of prime
+#define K (64) // degree of polynomial
+#define E (11)   // bit size of prime
 #define DEG N*K // set (K * E) < N
 #define T (K / 2) // weight of error vector
 #define Q K*2
